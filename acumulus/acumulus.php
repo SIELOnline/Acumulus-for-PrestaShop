71697a5db579269003a3877ef62032ee4920e280
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2015-11-07 18:12:43 +0100

    4.1.1

--- acumulus/acumulus.php
+++ acumulus/acumulus.php
@@ -37,7 +37,7 @@ class Acumulus extends Module {
    *
    * @var string
    */
-  public static $module_version = '4.0.2';
+  public static $module_version = '4.1.1';
 
   /** @var array */
   protected $options = array();
